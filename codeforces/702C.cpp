fc632d07650d1f49298d3b0b45e579f82ccca566
petkub <petkub2543@gmail.com>
2026-01-16 09:51:18 +0700

    update

--- codeforces/702C.cpp
+++ codeforces/702C.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
+const int INF = 4e18;
 
 int32_t main()
 {
@@ -8,7 +9,26 @@ int32_t main()
     ios::sync_with_stdio(0);
 
     int n,m; cin>>n>>m;
+    vector<int> tower(n),net(m);
     
-
+    for(int i=0;i<n;i++) cin>>tower[i];
+    for(int i=0;i<m;i++) cin>>net[i];
+    vector<int> r;
+    for(int i=0;i<n;i++)
+    {
+        auto lb = lower_bound(net.begin(),net.end(), tower[i]);
+        int dist = 4e18;
+        if (lb != net.end())
+        {
+            dist = min(dist, abs(*lb-tower[i]));
+        }
+        if (lb != net.begin())
+        {
+            lb--;
+            dist = min(dist, abs(*lb-tower[i]));
+        }
+        r.push_back(dist);
+    }
+    cout<<*max_element(r.begin(),r.end());
     return 0;
 }
